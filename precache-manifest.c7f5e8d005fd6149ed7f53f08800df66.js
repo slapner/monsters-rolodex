76d4a2c55478266512e8fed65b8d9cb75f9c03b8
slapner <bclap4@gmail.com>
2019-12-20 16:31:42 -0500

    Updates

---
+++ precache-manifest.c7f5e8d005fd6149ed7f53f08800df66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0f96b4f89cc61fd717ab9fbbcf0c8b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ba1232dc5e248515c2ad",
+    "url": "/monsters-rolodex/static/css/main.1024c57d.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba1232dc5e248515c2ad",
+    "url": "/monsters-rolodex/static/js/main.4a747092.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
